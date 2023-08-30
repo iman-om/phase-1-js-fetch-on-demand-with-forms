97929d0baa87c77a6a394677f9de3b73cfcac0e5
imane omari <imane3.omari@gmail.com>
2023-08-30 21:38:55 +0100

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
-const init = () => {
+inputForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const input = document.querySelector("input#searchByID");
+    const movieId = input.value;
   
-}
-
-document.addEventListener('DOMContentLoaded', init);
+    fetch(`http://localhost:3000/movies/${movieId}`)
+      .then((response) => response.json())
+      .then((data) => {
+        const titleElement = document.querySelector("section#movieDetails h4");
+        const summaryElement = document.querySelector("section#movieDetails p");
+  
+        titleElement.innerText = data.title;
+        summaryElement.innerText = data.summary;
+      });
+  });
+  
